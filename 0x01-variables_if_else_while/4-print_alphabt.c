31df4e4d4092741227a3124a36fcaca77f6bc59e
daod2003 <1253@holbertonschool.com>
2019-09-19 21:02:14 +0000

    add 4-print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int a = 97;
+while (a < 122)
+{
+if (a == 101 || a == 113)
+{
+a++;
+}
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
