05ce2d9f699c6f6f5800a7c956480bdffb187868
daod2003 <1253@holbertonschool.com>
2019-09-19 21:44:12 +0000

    add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int a = 122;
+
+for ( ; a > 96; a--)
+	putchar(a);
+putchar('\n');
+return (0);
+}
