db369a62859840cdae1bfddfc9f6df56fe857e98
daod2003 <1253@holbertonschool.com>
2019-09-20 00:22:33 +0000

    Modified 10-print

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,11 +9,11 @@ int main(void)
 int a = 48;
 int b = 48;
 
-while (a <58 && b <58)
+while (a < 58 && b < 58)
 {
 	putchar(a);
 	putchar(b);
-	if(a <57 || b < 57)
+	if (a < 57 || b < 57)
 	{
 		putchar(',');
 		putchar(' ');
