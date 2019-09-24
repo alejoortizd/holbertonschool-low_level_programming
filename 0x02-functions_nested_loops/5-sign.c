3fef828cdd8462a1d214a5b0d1526c5b01c32b86
daod2003 <1253@holbertonschool.com>
2019-09-24 12:00:09 +0000

    add 5-sing.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_sign(int n) - checks for integer
+ *
+ * @n: parametre to check
+ *
+ * Return: 1 or 0.
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar(48);
+	return (0);
+	}
+	else
+	{
+	_putchar(45);
+	return (-1);
+	}
+}
