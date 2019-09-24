3b1a734a73a37cf959fc700191a4aa6a481b154a
daod2003 <1253@holbertonschool.com>
2019-09-24 13:26:48 +0000

    Add 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @i: parametre to check.
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int i)
+{
+	int n = i % 10;
+	int m;
+	if (n < 0)
+		n = -n;
+	m = '0' + n;
+	_putchar(m);
+	return (n);
+}
