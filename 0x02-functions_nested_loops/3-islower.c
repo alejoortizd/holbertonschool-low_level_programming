89652920c7c70fadb8fcc6b342a07768629672b7
daod2003 <1253@holbertonschool.com>
2019-09-24 04:20:39 +0000

    add 4-isapha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 #include <ctype.h>
 /**
- * main - check the code for Holberton School students.
+ * _islower - check the code for Holberton School students.
  *
  * @c: parametre to check!
  * Return: Always 0.
  */
-
 int _islower(int c)
 {
 	if (islower(c))
