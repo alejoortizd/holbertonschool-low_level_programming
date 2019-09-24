ddc321d10e337073e8955dfa42aecaebe921e478
daod2003 <1253@holbertonschool.com>
2019-09-24 04:57:37 +0000

    modified 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
-
+/**
+ * print_alphabet - entry point
+ *
+ * Description: Prints alphabet with _putchar
+ *
+ * Return: always 0
+ */
 void print_alphabet(void)
 {
 	int a = 'a';
 	int b = 'z';
 
-	for( ; a <= b ; a++)
+	for ( ; a <= b ; a++)
 		_putchar(a);
 	_putchar('\n');
 }
