8a1bd0751f7a88c4c7920f5faa91ddb73a3e7ffc
daod2003 <1253@holbertonschool.com>
2019-09-26 21:36:35 +0000

    Add 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,7 +16,7 @@ void print_triangle(int size)
 		{
 			for ( ; j < size; j++)
 			{
-				if (((i + j) >= (size-1)))
+				if (((i + j) >= (size - 1)))
 					_putchar('#');
 				else
 					_putchar(' ');
