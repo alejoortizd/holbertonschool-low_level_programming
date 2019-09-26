457c86ad111899fcb95f0b792f8e5b757cfd0a77
daod2003 <1253@holbertonschool.com>
2019-09-26 19:19:24 +0000

    add 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_diagonal - entry point
+ * Descripton: print diagonal
+ * @n: parametre to check
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n > 0)
+	{
+		for ( ; i < n; i++)
+		{
+			for ( ; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+			j = 0;
+		}
+	}
+	else
+		_putchar('\n');
+}
