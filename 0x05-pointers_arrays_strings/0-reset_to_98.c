2436b2fec497391a56c12fd8ca8554d2d0a454f9
daod2003 <1253@holbertonschool.com>
2019-10-01 11:40:30 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Entry point
+ * Description: Function that reset the pointer
+ * @n: parametre to check
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
