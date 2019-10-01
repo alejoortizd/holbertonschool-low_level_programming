5f30ffe866b079960d23ff637dc4a5886ad49a4f
daod2003 <1253@holbertonschool.com>
2019-10-01 20:48:36 +0000

    finish all mandatory

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,9 +2,25 @@
 /**
  * rev_string - Entry point
  * Description: function that prints Holberton and notrebloH
- *
+ * @s: parametre to check
  * Return: always a void
  */
 void rev_string(char *s)
 {
-	int a = 0;
+	int i, temp, x;
+
+	x = 0;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	i--;
+	while (x < i)
+	{
+		temp = s[x];
+		s[x] = s[i];
+		s[i] = temp;
+		x++;
+		i--;
+	}
+}
