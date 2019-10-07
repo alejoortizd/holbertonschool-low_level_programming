159f5cd519ef37f5947fd696726113ff2ca6354e
daod2003 <1253@holbertonschool.com>
2019-10-07 23:14:18 +0000

    Add 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strpbrk - Entry point
+ * Description: Function that return the length
+ * @s: parametre to check
+ * @accept: parametre to check
+ * Return: unsigned
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0;
+
+	for (i = 0 ; s[0] ; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j])
+			return (s +1);
+	}
+	return (0);
+}
