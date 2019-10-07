49fbc099ef3731f6acd106242ff6edb227757de6
daod2003 <1253@holbertonschool.com>
2019-10-07 20:19:35 +0000

    Modified file 2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,16 +2,20 @@
 /**
  * _strchr - Entry point
  * Description: Function that return the memory area pinted
- * @n: parametre to check
- * @dest: parametre to check
- * @src: parametre to check
+ * @s: parametre to check
+ * @c: parametre to check
  * Return: s
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != 0 && s[i] != c ; i++)
-		;
-	return (s + i);
+	for ( ; *(s + i) ; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
 }
