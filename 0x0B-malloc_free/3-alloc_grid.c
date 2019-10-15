7da5b384dcc5bc7c94b8672515bdb46663f18a61
daod2003 <1253@holbertonschool.com>
2019-10-15 23:38:53 +0000

    Add the F file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **k;
 
-	if(width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	k = (int **)malloc(height * sizeof(int *));
 	if (k == NULL)
