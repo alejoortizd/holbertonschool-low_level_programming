9518effdc5e4bc64cf2a7f11911670fdbb7c84de
daod2003 <1253@holbertonschool.com>
2019-10-15 20:41:28 +0000

    add 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * str_concat - Entry Point
  * Description: function that concatenates 2 strings
@@ -10,11 +9,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr = malloc(strlen(s1) + strlen(s2) + 1);
+	int i, j, c1, c2;
+	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0 ; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	ptr = malloc((sizeof(char) * i + sizeof(char) * j) + 1);
+	if (ptr == NULL)
+	{
 		return (NULL);
-	strcpy(ptr, s1);
-	strcat(ptr, s2);
-	return (ptr);
+	}
+	else
+	{
+		for (c1 = 0; c1 < i; c1++)
+			ptr[c1] = s1[c1];
+		for (c2 = 0; c2 < j; c1++, c2++)
+			ptr[c1] = s2[c2];
+		return (ptr);
+	}
 }
