32f6fb6a2b5e3d4d0440466352a979c82d2f7c6d
daod2003 <1253@holbertonschool.com>
2019-10-16 04:40:45 +0000

    add the last mandatory exercise

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - Entry Point
+ * @ac: parametre to checks
+ * @av: parametre to check
+ * Return:pointer to new string, or NULL if error
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, l = 0;
+	char *s;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			l++;
+		l++;
+	}
+	l++;
+	s = malloc(n * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		s[k] = '\n';
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
+}
