fa69ab785c407d8d37432fd45e9fe12dccfaeed3
daod2003 <1253@holbertonschool.com>
2019-10-17 18:49:22 +0000

    first commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * malloc_checked - Entry Point
+ * Description: function that return a void
+ * @b: parametre to check
+ * Return: void
  */
 void *malloc_checked(unsigned int b)
 {
 	void *c = malloc(b);
 
-	if (b == NULL)
+	if (c == NULL)
 		exit(98);
-	return(c);
+
+	return (c);
 }
