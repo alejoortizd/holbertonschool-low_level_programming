fe7e93cc6e57dcfa721a93bb41ce2c093cc705da
daod2003 <1253@holbertonschool.com>
2019-10-22 02:00:51 +0000

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - Entry Point
+ * @name: parametre to check
+ * @age: parametre to check
+ * @owner: parametre to check
+ * Description: function with long description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_h;
+int _putchar (char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_h *new_dog(char *name, float age, char *owner);
+void free_dog(dog_h *d);
+#endif
