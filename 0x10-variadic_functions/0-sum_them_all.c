7eccc562ee39401dd9c51f13bcd26f5825ada44a
daod2003 <1253@holbertonschool.com>
2019-10-26 20:50:38 +0000

    last chance

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,10 +10,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-
 	va_list valist;
+	double sum = 0.0;
 	unsigned int i;
-	double sum = 0;
+
+	if (n == 0)
+		return (0);
 
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
