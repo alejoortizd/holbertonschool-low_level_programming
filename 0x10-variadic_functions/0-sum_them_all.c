4263d925b574e2c50fa39cb918c61415e8b3deb2
daod2003 <1253@holbertonschool.com>
2019-10-26 20:55:49 +0000

    fixed the FF0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,25 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 /**
- * sum_them_all - Entry Point
- * Description: function that sum
- * @n: parametre to check
- * Return: sum
+ * sum_them_all - Entry point
+ *
+ * Description: write a function that returns the sum of all its parameters
+ * @n: Is the parameter to be checked
+ * Return: Always 0 (Success)
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
 	double sum = 0.0;
-	unsigned int i;
+	unsigned int k;
 
 	if (n == 0)
 		return (0);
 
 	va_start(valist, n);
-	for (i = 0; i < n; i++)
+	for (k = 0; k < n; k++)
 	{
 		sum += va_arg(valist, int);
 	}
