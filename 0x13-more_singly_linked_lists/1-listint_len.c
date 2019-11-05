c1ae57980272455c1814d0da4318d75681f5e428
daod2003 <1253@holbertonschool.com>
2019-11-05 18:28:22 +0000

    first commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - entry point
+ * @h: parametre to check
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - entry point
+ * @h: parametre to check
+ * Return: number of nodes
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
