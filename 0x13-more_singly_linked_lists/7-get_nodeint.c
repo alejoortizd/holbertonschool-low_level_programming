b795301669a7a95798ffdbe4d03dc956f5f2253d
daod2003 <1253@holbertonschool.com>
2019-11-06 00:22:03 +0000

    add 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
- * pop_listint - entry point
- * @head: parametre to check
+ * get_nodeint_at_index - entry point
+ * @head: parameter to check
+ * @index: parameter to check
  * Return: number of nodes
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
