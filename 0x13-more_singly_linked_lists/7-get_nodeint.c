8637a5e584026bb9881347d293d7fd10c63a3c74
daod2003 <1253@holbertonschool.com>
2019-11-06 00:20:38 +0000

    add 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 	t = head;
-	if (t == NULL)
+	if (head == NULL)
 		return (NULL);
-	while (i < index)
+	while (i < index && t != NULL)
 	{
 		t = t->next;
 		i++;
 	}
-	return (t);
+		return (t);
 }
