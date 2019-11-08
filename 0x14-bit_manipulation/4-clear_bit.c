50e99892f0ced1b3b4a9f9ac30c601fe08635ab6
daod2003 <1253@holbertonschool.com>
2019-11-08 01:52:34 +0000

    add 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * clear_bit - Entry point
+ * @n: parameter to check
+ * @index: parameter to check
+ * Return: 1,-1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = ~(1UL << index);
+	if (index > 63)
+		return (-1);
+	*n = *n & mask;
+	return (1);
+}
