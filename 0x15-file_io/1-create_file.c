7cb0f4424de0a30c5e5c2f5bca29a11e57b8582e
daod2003 <1253@holbertonschool.com>
2019-11-13 21:19:04 +0000

    add 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 
 	int fdf = 0, frd = 0, i = 0, j = 0;
 
-	if (text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	fdf = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fdf == -1)
