9fba7aa76eaa052b5d40f36b50bc94118e792784
daod2003 <1253@holbertonschool.com>
2019-11-13 21:15:35 +0000

    add 1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * create_file - Entry point
+ * @filename: parameter to check
+ * @text_content: parameter to check
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+	int fdf = 0, frd = 0, i = 0, j = 0;
+
+	if (text_content == NULL)
+		return (-1);
+	fdf = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fdf == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		frd = write(fdf, text_content, i)
+			if (frd == -1)
+				return (-1);
+	}
+	j = close(fdf);
+	if (j == -1)
+		return (-1);
+	return (1);
+}
