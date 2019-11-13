9563cff19b69ea922779040f53a363b46c06033b
daod2003 <1253@holbertonschool.com>
2019-11-13 21:17:36 +0000

    add 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
-		frd = write(fdf, text_content, i)
-			if (frd == -1)
-				return (-1);
+		frd = write(fdf, text_content, i);
+		if (frd == -1)
+			return (-1);
 	}
 	j = close(fdf);
 	if (j == -1)
