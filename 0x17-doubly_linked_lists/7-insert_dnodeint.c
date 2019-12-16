4a59fcb04104adb6f06f38f092fb95a5d1755098
daod2003 <1253@holbertonschool.com>
2019-12-16 04:36:07 +0000

    add 7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - entry point
+ * @head: parameter to check
+ * @idx: parameter to check
+ * @n: parameter to check
+ * Return: new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *headcopy;
+	unsigned int i;
+
+	headcopy = *h;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	while (headcopy != NULL && headcopy->prev != NULL)
+	{
+		headcopy = headcopy->prev;
+		*h = (*h)->prev;
+	}
+	if (idx == 0)
+	{
+		free(new);
+		return (add_dnodeint(h, n));
+	}
+
+	for (i = 0; (i < idx - 1) && headcopy != NULL; i++)
+		headcopy = headcopy->next;
+	if (headcopy == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (headcopy->next == NULL)
+	{
+		new->next = NULL;
+		new->prev = headcopy;
+		headcopy->next = new;
+	} else
+	{
+		new->next = headcopy->next;
+		new->prev = headcopy;
+		headcopy->next->prev = new;
+		headcopy->next = new;
+	}
+	return (new);
+}
