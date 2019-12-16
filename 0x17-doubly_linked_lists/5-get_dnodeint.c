f51437f6338cccf2ca2adc46839df1234d09534e
daod2003 <1253@holbertonschool.com>
2019-12-16 04:05:58 +0000

    add 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - entry point
+ * @head: parameter to check
+ * @index: parameter to check
+ * Return: number of nodes
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	tmp = head;
+	if (head == NULL)
+		return (NULL);
+	while (i < index && tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (tmp);
+}
