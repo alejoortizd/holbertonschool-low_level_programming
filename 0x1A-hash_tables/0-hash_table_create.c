2c137ae777efdc73f5be4710004f0d9353c30bbf
daod2003 <1253@holbertonschool.com>
2020-01-18 17:55:31 +0000

    add header and file 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_fun.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *n_hash_table;
+	unsigned long int i;
+
+	n_hash_table = malloc(sizeof(hash_table_t));
+	if (n_hash_table == NULL)
+		return (NULL);
+	n_hash_table->size = size;
+	n_hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (n_hash_table->array == NULL)
+	{
+		free(n_hash_table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		n_hash_table->array[i] = NULL;
+	return n_hash_table;
+}
