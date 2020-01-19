cd051d99d96d31fe49c24072fd657f916ca5917d
daod2003 <1253@holbertonschool.com>
2020-01-19 21:34:09 +0000

    modified file 4-hash_table_get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-		key == NULL || strlen(key) == 0 || value == NULL)
+		key == NULL || strlen(key) == 0)
 		return (NULL);
 	i = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[i];
