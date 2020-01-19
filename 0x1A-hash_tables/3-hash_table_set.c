414ade2f7fbd886ff2a718c8606e959fd6c1b3f4
daod2003 <1253@holbertonschool.com>
2020-01-19 16:41:33 +0000

    add 3-hash_table.c and modified README.md

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,68 @@
+#include "hash_tables.h"
+/**
+ * create_hnode - Entry Point
+ * @key: parameter to check
+ * @value: parameter to check
+ * Return: the new node
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
+ * hash_table_set - Entry Point
+ * @ht: parameter to check
+ * @key: parameter to check
+ * @value: parameter to check
+ * Return: add element to the hash table
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *node, *tmp;
+	char *nvalue;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+		key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	i = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			nvalue = strdup(value);
+			if (nvalue == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = nvalue;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	node = create_node(key, value);
+	if (node == NULL)
+		return (0);
+	node->next = ht->array[i];
+	ht->array[i] = node;
+	return (1);
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,1 +1,20 @@
-This is the README.md
+# 0x19. C - Hash tables
+## About
+An introductory project on:
+- Hash functions
+- How to use hash tables
+- Dealing with collisions
+## Requirements
+- Ubuntu 14.04
+- gcc 4.8.4
+## File Descriptions
+**[hash_tables.h](hash_tables.h)** - header file containing all function prototypes and the definitions for types `hash_node_t`, `hash_table_t`, `shash_node_t`, and `shash_table_t`.
+
+### Mandatory
+**[0-hash_table_create.c](0-hash_table_create.c)** - function that creates a hash table.
+
+**[1-djb2.c](1-djb2.c)** - hash function that implements the djb2 algorithm.
+
+**[2-key_index.c](2-key_index.c)** - function that returns the index of a key.
+
+**[3-hash_table_set.c](3-hash_table_set.c)** - function that adds an element to the hash table.
