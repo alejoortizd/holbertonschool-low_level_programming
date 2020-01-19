ae57b754d1cd5d71a0a5222c321767d1b1ab1889
daod2003 <1253@holbertonschool.com>
2020-01-19 21:33:17 +0000

    add file 4-hash_table_get

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - Entry Point
+ * @key: parameter to check
+ * @ht: parameter to check
+ * Return: the table
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+		key == NULL || strlen(key) == 0 || value == NULL)
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp != NULL)
+	{
+		if (strcpy(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,5 +37,5 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *create_hnode(const char *key, const char value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif
