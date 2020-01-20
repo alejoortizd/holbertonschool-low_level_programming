7fbcefe7e2442206581f6562192d7846ae38af50
daod2003 <1253@holbertonschool.com>
2020-01-20 00:32:45 +0000

    fixed code '=' for '=='

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,29 @@
 #include "hash_tables.h"
 /**
- * hash_table_get - Entry Point
- * @key: parameter to check
+ * hash_table_print - Entry Point
  * @ht: parameter to check
  * Return: the table
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *tmp;
-    char sentinel = 0;
+	unsigned long int i;
+	hash_node_t *tmp;
+	char sentinel = 0;
 
-    if (ht = NULL || ht->array == NULL)
-        return;
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        tmp = ht->array[i];
-        while (tmp != NULL)
-        {
-            if (sentinel == 1)
-                printf(", ");
-            printf("'%s': '%s'", tmp->key, tmp->value);
-            sentinel = 1;
-            tmp = tmp->next;
-        }
-    }
-    printf("}\n");
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (sentinel == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			sentinel = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
 }
