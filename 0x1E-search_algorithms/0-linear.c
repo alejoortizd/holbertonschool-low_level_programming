a6891f28ec3a41b5678c64f957e0a039c57fea9a
daod2003 <1253@holbertonschool.com>
2020-04-16 05:31:40 +0000

    add 0 and header

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * linear_search: entry point
+ * @array: parameter to be check
+ * @size: parameter to be check
+ * @value: parameter to be check
+ * Return: a value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
